987faabf5b12fc6a992b950b0ac87d61d6a75f14
WijayaGanda <wijayatyo49@gmail.com>
2024-05-29 19:36:16 +0700

    first commit

---
+++ resources/views/layouts/nav.blade.php
@@ -0,0 +1,13 @@
+
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <a class="nav-link active" aria-current="page" href="{{route('home')}}">Home</a>
+                    <a class="nav-link" href="#">Login</a>
+                    <a class="nav-link" href="#">List</a>
+                    <a class="nav-link" href="#">Logout</a>
+                </div>
+            </div>
+        </div>
+    </nav>
