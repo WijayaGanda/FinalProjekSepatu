993bad4f3a6879a665285e8ac6c22ab32628cbac
Pavita Pramestri <pavita.vita04@gmail.com>
2024-05-30 17:37:09 +0700

    Halaman create. edit, nav

---
+++ resources/views/barang/edit.blade.php
@@ -0,0 +1,69 @@
+@extends('layouts.nav')
+
+@section('content')
+    <div class="container-sm d-flex justify-content-center my-5">
+        <div class="card" style="width: 34rem;">
+            <div class="card-body p-3">
+                <div class="mb-2 text-center">
+                    <i class="bi bi-pencil-square h3"></i>
+                    <h4>Edit Catalog</h4>
+                </div>
+                <hr>
+                <div class="col mb-3">
+                    <label for="brand" class="form-label">Brand</label>
+                    <input class="form-control @error('brand') is-invalid @enderror" type="text" name="brand"
+                        id="brand" value="{{ old('brand') }}" placeholder="Insert Brand Name">
+                </div>
+                <div class="col mb-3">
+                    <label for="stock" class="form-label">Stock</label>
+                    <input class="form-control @error('stock') is-invalid @enderror" type="number" name="stock"
+                        id="stock" value="{{ old('stock') }}" placeholder="Insert Stock">
+                </div>
+                <div class="row">
+                    <div class="col-md-6 mb-3">
+                        <label for="category" class="form-label">Category</label>
+                        <div class="dropdown">
+                            <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown"
+                                aria-expanded="false">
+                                Choose Category
+                            </button>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="#">Man</a></li>
+                                <li><a class="dropdown-item" href="#">Woman</a></li>
+                                <li><a class="dropdown-item" href="#">Kids</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-md-6 mb-3">
+                        <label for="supplier" class="form-label">Supplier</label>
+                        <div class="dropdown">
+                            <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown"
+                                aria-expanded="false">
+                                Choose Supplier
+                            </button>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="#">Nike</a></li>
+                                <li><a class="dropdown-item" href="#">Adidas</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col mb-3">
+                    <label for="formFile" class="form-label"><i class="bi bi-filetype-jpg h5"></i> Image</label>
+                    <input class="form-control" type="file" id="formFile">
+                </div>
+                <div class="row mt-4 justify-content-center">
+                    <div class="col-md-6 d-grid mb-3">
+                        <a href="{{ route('home') }}" class="btn btn-outline-danger btn-lg mt-3"><i
+                                class="bi bi-x-circle"></i> Cancel</a>
+                    </div>
+
+                    <div class="col-md-6 d-grid mb-3">
+                        <button type="submit" class="btn btn-outline-success btn-lg mt-3"><i class="bi bi-floppy"></i>
+                            Save</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,13 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
 
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-        <div class="container-fluid">
-            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                <div class="navbar-nav">
-                    <a class="nav-link active" aria-current="page" href="{{route('home')}}">Home</a>
-                    <a class="nav-link" href="#">Login</a>
-                    <a class="nav-link" href="#">List</a>
-                    <a class="nav-link" href="#">Logout</a>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite('resources/sass/app.scss')
+</head>
+
+<body>
+    <div class="container">
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#"><i class="bi bi-box-seam"></i> SB</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <div class="navbar-nav">
+                        <a class="nav-link {{ request()->is('/') ? 'active' : '' }}" aria-current="page"
+                            href="{{ route('home') }}">Home</a>
+                        <a class="nav-link {{ request()->is('login') ? 'active' : '' }}" aria-current="page"
+                            href="/login">Login</a>
+                        <a class="nav-link {{ request()->is('list') ? 'active' : '' }}" aria-current="page"
+                            href="/list">List</a>
+                        <a class="nav-link" href="/logout">Logout</a>
+                    </div>
                 </div>
             </div>
+        </nav>
+        <div class="content my-5">
+            @yield('content')
         </div>
-    </nav>
+    </div>
+
+    @vite('resources/js/app.js')
+</body>
+
+</html>
