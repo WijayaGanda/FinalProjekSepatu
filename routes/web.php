4ac705e91d35d3cd0a0ea10a8d7cae0fde61f8de
naulikhalila <naulikhalila@gmail.com>
2024-05-30 20:04:25 +0700

    login nauli

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,8 +15,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('login');
+Route::get('/auth', function () {
+    return view('auth.login');
 });
 
 Route::get('home',[HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
