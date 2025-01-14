ea5b2b4d4c3d36e8876145f3f29763ed78bbe1ac
WijayaGanda <wijayatyo49@gmail.com>
2025-01-15 01:03:53 +0700

    fix

--- resources/views/customerlayouts/navcustomer.blade.php
+++ resources/views/customerlayouts/navcustomer.blade.php
@@ -19,7 +19,13 @@
 
 <nav class="navbar navbar-expand-lg">
     <div class="container-fluid">
-        <a class="navbar-brand" href="{{ route('pelanggan.homecustomer') }}"><i class="bi bi-box-seam"></i> SB</a>
+        <a class="navbar-brand" href="{{ route('pelanggan.homecustomer') }}"><i class="bi bi-box-seam"></i>
+            @auth
+                Halo, {{ Auth::user()->name }}
+            @else
+                SB
+            @endauth
+        </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
             aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
